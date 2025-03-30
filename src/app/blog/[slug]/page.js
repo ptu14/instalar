9820204713be2fa9691cce85ab8b718f08898caf
mateusz <danilukmateusz92@gmail.com>
2025-03-30 20:48:55 +0200

    poprawka blogu

--- src/app/blog/[slug]/page.js
+++ src/app/blog/[slug]/page.js
@@ -2,6 +2,7 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import axios from "axios";
+import "../blog.css";
 
 import Navbar from "../../components/navbar/navbar";
 import KomplexFooter from "@/app/components/footer/komplexFooter";
