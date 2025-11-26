5c2f4d54175e6612a04ef6db077da009e6b3139c
mateusz <danilukmateusz92@gmail.com>
2025-11-26 09:46:57 +0100

    refactor: Remove unused font imports and improve SCSS selector readability.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,4 @@
-import { Poppins, Ubuntu, Teko, Noto_Sans, Libre_Baskerville, Kaushan_Script, Lexend } from 'next/font/google'
+import { Lexend } from 'next/font/google'
 import 'bootstrap/dist/css/bootstrap.css'
 import './assets/scss/style.scss'
 import './assets/css/materialdesignicons.min.css'
@@ -10,38 +10,6 @@ const lexend = Lexend({
   subsets: ['latin'],
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
   variable: '--font-lexend',
-})
-
-const poppins = Poppins({
-  subsets: ['latin'],
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-  variable: '--font-poppins',
-})
-
-const ubuntu = Ubuntu({
-  subsets: ['latin'],
-  weight: ['300', '400', '500', '700'],
-  variable: '--font-ubuntu',
-})
-const teko = Teko({
-  subsets: ['latin'],
-  weight: ['300', '400', '500', '700'],
-  variable: '--font-teko',
-})
-const noto = Noto_Sans({
-  subsets: ['latin'],
-  weight: ['400', '700'],
-  variable: '--font-noto',
-})
-const libre = Libre_Baskerville({
-  subsets: ['latin'],
-  weight: ['400', '700'],
-  variable: '--font-libre',
-})
-const kaushan = Kaushan_Script({
-  subsets: ['latin'],
-  weight: ['400'],
-  variable: '--font-kaushan',
 })
 
 export const metadata = {
@@ -147,7 +115,7 @@ export default function RootLayout({ children }) {
       </head>
       <Analytics />
       <SpeedInsights />
-      <body className={`${lexend.variable} ${ubuntu.variable} ${teko.variable} ${noto.variable} ${libre.variable} ${kaushan.variable}`}>
+      <body className={`${lexend.variable}`}>
         {children}
         <div className="position-fixed bottom-0 start-0 m-3" style={{ zIndex: 9999 }}>
           <GoogleBadge />
