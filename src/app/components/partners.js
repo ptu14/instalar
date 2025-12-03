fb287e59befb2d7c1788271fa6d52f1a77883518
mateusz <danilukmateusz92@gmail.com>
2025-12-03 21:07:40 +0100

    feat: Update partner logos by adding Mitsubishi and Sigenergy, removing Foxess, and refactor image data to include alt attributes.

--- src/app/components/partners.js
+++ src/app/components/partners.js
@@ -6,25 +6,26 @@ import { Autoplay } from 'swiper/modules';
 
 import 'swiper/css';
 
-export default function Partners(){
+export default function Partners() {
     let clientImg = [
-        '/images/partners/daikin.svg',
-        '/images/partners/rotenso.webp',
-        '/images/partners/gree.webp',
-        '/images/partners/huawei.webp',
-        '/images/partners/jasolar.webp',
-        '/images/partners/panasonic.svg',
-        '/images/partners/longi.png',
-        '/images/partners/sofar.webp',
-        '/images/partners/trinasolar.jpg',
-        '/images/partners/jinko.png',
-        '/images/partners/deye.webp',
-        '/images/partners/solaredge.webp',
-        '/images/partners/foxess.webp',
+        { src: '/images/partners/daikin.svg', alt: 'Daikin' },
+        { src: '/images/partners/mitsubishi.webp', alt: 'Mitsubishi' },
+        { src: '/images/partners/sigenergy.webp', alt: 'Sigenergy' },
+        { src: '/images/partners/gree.webp', alt: 'Gree' },
+        { src: '/images/partners/huawei.webp', alt: 'Huawei' },
+        { src: '/images/partners/jasolar.webp', alt: 'JA Solar' },
+        { src: '/images/partners/panasonic.svg', alt: 'Panasonic' },
+        { src: '/images/partners/longi.png', alt: 'LONGi' },
+        { src: '/images/partners/sofar.webp', alt: 'Sofar' },
+        { src: '/images/partners/trinasolar.jpg', alt: 'Trina Solar' },
+        { src: '/images/partners/jinko.png', alt: 'Jinko Solar' },
+        { src: '/images/partners/rotenso.webp', alt: 'Rotenso' },
+        { src: '/images/partners/deye.webp', alt: 'Deye' },
+        { src: '/images/partners/solaredge.webp', alt: 'SolarEdge' },
     ]
 
-    return(
-        <div className="container-fluid" style={{ minHeight: '120px' }}> 
+    return (
+        <div className="container-fluid" style={{ minHeight: '120px' }}>
             <Swiper
                 modules={[Autoplay]}
                 loop={true}
@@ -33,7 +34,7 @@ export default function Partners(){
                     disableOnInteraction: false,
                 }}
                 slidesPerView={'auto'}
-                spaceBetween={30} 
+                spaceBetween={30}
                 breakpoints={{
                     0: {
                         slidesPerView: 2,
@@ -59,12 +60,12 @@ export default function Partners(){
                 className="partners-carousel"
                 style={{ height: '120px' }}
             >
-                {clientImg.map((item,index)=>{
-                    return(
+                {clientImg.map((item, index) => {
+                    return (
                         <SwiperSlide key={index} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
                             <div className="text-center">
                                 <div className="d-flex align-items-center justify-content-center h-100">
-                                    <Image src={item} width={0} height={0} sizes="100vw" style={{width:'auto', maxHeight:'80px', minHeight:'40px'}} className="img-fluid" alt=""/>
+                                    <Image src={item.src} width={0} height={0} sizes="100vw" style={{ width: 'auto', maxHeight: '80px', minHeight: '40px' }} className="img-fluid" alt={item.alt} />
                                 </div>
                             </div>
                         </SwiperSlide>
